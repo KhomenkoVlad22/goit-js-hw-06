af84d106e7eeb489a0ed531e4140beb52dfb73b6
KhomenkoVlad22 <98429095+KhomenkoVlad22@users.noreply.github.com>
2022-10-25 14:40:19 +0300

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
+let button = document.querySelector('.chage-color');
+let color = document.querySelector('.color');
+
+button.addEventListener('click', () => {
+  let currentColor = getRandomHexColor();
+
+  document.body.style.backgroundColor = currentColor;
+  color.textContent = currentColor;
+
+});
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
