71e03d9afc3081001c4f54d9162381a214f9878d
KhomenkoVlad22 <98429095+KhomenkoVlad22@users.noreply.github.com>
2022-10-25 13:43:22 +0300

    Update task 02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector("#ingredients");
+
+const arr = [];
+for (let i = 0; i < ingredients.length; i += 1) {
+  const newItem = document.createElement("li");
+  newItem.classList.add('item');
+  newItem.textContent = ingredients[i];
+  console.log(newItem);
+  arr.push(newItem);
+}
+list.append(...arr);
